7ae85f65df130380101744ef2824f945940b925f
tejas93098 <sangitamarudwar46@gmail.com>
2025-09-15 23:11:03 +0530

    Update api.ts

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = 'https://myproject-backend-e0bm.onrender.com/api';
+
 
 const api = axios.create({
   baseURL: API_BASE_URL,
